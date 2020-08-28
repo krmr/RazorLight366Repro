0ffb8a67b2d9cacb0774a8d0d362f05f1df3e214
krmr <michael@krmr.org>
2020-08-28 17:37:14 +0200

    adding solution files

---
+++ RazorLightTest/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace RazorLightTest
+{
+    using System;
+    using System.Globalization;
+    using System.Threading.Tasks;
+
+    using RazorLight;
+
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            CultureInfo.CurrentUICulture = new CultureInfo("en-us");
+
+            var engine = new RazorLightEngineBuilder()
+                .UseEmbeddedResourcesProject(typeof(Program))
+                .UseMemoryCachingProvider()
+                .Build();
+
+            var template = "Hello, @Model.Name. Welcome to RazorLight repository";
+            var model = new ViewModel { Name = "John Doe" };
+
+            var result = await engine.CompileRenderStringAsync("templateKey", template, model);
+            Console.WriteLine(result);
+        }
+
+        public class ViewModel
+        {
+            public string Name { get; set; }
+        }
+    }
+}
